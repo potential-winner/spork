5ee6fd503c27896dbe9a14eede525b0c8ded0271
Abdumuminov, Umedzhon <ovegug@gmail.com>
2020-03-21 15:58:44 +0300

    Added failing test

--- spork/hello.py
+++ spork/hello.py
@@ -1,2 +1,5 @@
 def hello_to(name: str) -> str:
     return "Hello, " + name
+
+def hello_world_and_to(name: str) -> str:
+    return "Hello, World and " + name
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,5 +1,9 @@
-from spork.hello import hello_to
+from spork.hello import hello_to, hello_world_and_to
 
 def test_hello_to_aziz():
     result = hello_to("aziz")
+    assert result == "Hello, aziz"
+
+def test_hello_world_and_to_aziz():
+    result = hello_world_and_to("aziz")
     assert result == "Hello, aziz"
