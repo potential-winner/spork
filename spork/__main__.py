9469328a323fa8f2d7d77256233ed1488f29c46b
Abdumuminov, Umedzhon <ovegug@gmail.com>
2020-03-25 23:32:42 +0300

    Setup bot and first command

--- spork/__main__.py
+++ spork/__main__.py
@@ -1,9 +1,26 @@
 from spork.hello import hello_to
+from telegram import Bot
+from telegram.ext import Updater, CommandHandler
+import sys
 
-def main():
-    name = input("What is your name?")
-    print(hello_to(name))
+PROXY = 'https://telegg.ru/orig/bot'
+
+def hello(update, context):
+    hello_message = hello_to(update.message.from_user.first_name)
+    update.message.reply_text(hello_message)
+
+
+def main(access_token: str):
+    bot = Bot(
+        token=access_token,
+        base_url=PROXY,  # delete it if connection via VPN
+    )
+    updater = Updater(bot=bot, use_context=True)
+    updater.dispatcher.add_handler(CommandHandler('hello', hello))
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == "__main__":
-    main()
+    access_token = sys.argv[1]
+    main(access_token)
